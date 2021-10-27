d4b7e4a5ac81f38d42b5081bc5101336cedee9c4
kanesada2 <guts.kk.89@gmail.com>
2021-10-28 00:04:03 +0900

    色取得して背景色変更まで

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <reader />
+</template>
+<script setup lang="ts">
+import reader from './components/reader.vue'
+</script>
+<style>
+</style>
---
+++ src/components/reader.vue
@@ -0,0 +1,48 @@
+<template>
+    <div :style="{ backgroundColor: rgba }">
+        <video ref="videoRef"></video>
+        <canvas ref="canvasRef"></canvas>
+        <div>
+            <button @click="snap">取得</button>
+            <div class="indicator">{{ rgba }}</div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { ref } from '@vue/reactivity'
+import { defineComponent, onMounted, watch } from '@vue/runtime-core'
+
+export default defineComponent({
+    setup() {
+        const canvasRef = ref<HTMLCanvasElement>()
+        const videoRef = ref<HTMLVideoElement>()
+        let rgba = ref<string>('')
+        
+        onMounted(()=>{
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                    videoRef.value.srcObject = stream
+                    videoRef.value?.play()
+                })
+            }
+        })
+        function snap() {
+            const context = canvasRef.value?.getContext('2d')
+            context?.drawImage(videoRef.value, 0, 0, 1, 1)
+            const colorData = context?.getImageData(0,0,1,1)
+            rgba.value = "rgba("+ colorData?.data + ")"
+            console.log(rgba.value)
+        }
+        return {
+            canvasRef, videoRef, rgba, snap
+        }
+    }
+})
+</script>
+<style scoped>
+.indicator {
+    color: #fff;
+    font-weight: bold;
+    font-size: 1.2rem;
+}
+</style>
